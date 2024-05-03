1a25625c6b51e20297d6cbd81372d625e9dce5d7
nichoandrew01 <nichoandrew01@gmail.com@email.com>
2024-05-03 18:03:30 +0800

    update pop up

--- src/app/vendor/upload/upload/PupUp.jsx
+++ src/app/vendor/upload/upload/PupUp.jsx
@@ -10,17 +10,11 @@ export default function PopUp({ isOpenPopUp, closePopUp }) {
          ${isOpenPopUp ? "block" : "hidden"}`}
       >
         <div className="flex items-center justify-center">
-          <div
-            className={`w-80 md:w-96 p-5 flex flex-col items-center justify-center bg-white border rounded absolute top-1/3 lg:ms-10 z-10 `}
-          >
+          <div className={`w-80 md:w-96 p-5 flex flex-col items-center justify-center bg-white border rounded absolute top-1/3 lg:ms-10 z-10 `}>
             <FaRegCircleCheck className="text-2xl mb-1" />
             <h3 className="font-bold text-2xl">Unggah Kursus</h3>
-            <p>Data kursus anda berhasil diunggah</p>
-            <button
-              type="button"
-              className="px-6 py-1.5 text-white bg-blue rounded mt-5"
-              onClick={closePopUp}
-            >
+            <p>Data kursus berhasil diunggah</p>
+            <button type="button" className="px-6 py-1.5 text-white bg-blue rounded mt-5" onClick={closePopUp}>
               Tutup
             </button>
           </div>
